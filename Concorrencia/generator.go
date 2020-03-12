9b6a8bdbd0142bd1d22dee33100d2f420e61d5a1
MayRegis <iabit.quantum@gmail.com>
2020-03-12 10:04:01 -0300

    incluindo generator.go

--- Concorrencia/generator.go
+++ Concorrencia/generator.go
@@ -8,12 +8,16 @@ import (
 )
 
 func titulo(urls ...string) <-chan string {
-	c := make(chan string, 2)
+	c := make(chan string)
 	for _, url := range urls {
+		//semelhante ao length
 		go func(url string){
 			resp, _ := http.Get(url)
+			//chama a estrutura da url
 			html, _ := ioutil.ReadAll(resp.Body)
-			r, _ := regexp.Compile("(.*?)")
+			//ioutil - leitura (manipulacao de buffer)
+			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
+			//expressao regular - autômato
 			c <- r.FindStringSubmatch(string(html))[1]
 		}(url)
 	}
@@ -22,7 +26,7 @@ func titulo(urls ...string) <-chan string {
 
 func main() {
 	t1 := titulo("https://www.cod3r.com.br", "https://www.google.com")
-	t2 := titulo("https://www.amazon.com", "https://www.youtube.com")
+	t2 := titulo("https://pt.wikipedia.org/wiki/Wikip%C3%A9dia:P%C3%A1gina_principal", "https://www.youtube.com")
 	log.Println("Primeiros:", <-t1, "|", <-t2)
 	log.Println("Segundos:", <-t1, "|", <-t2)
 }
